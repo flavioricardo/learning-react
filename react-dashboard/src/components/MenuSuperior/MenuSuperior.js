5d2b007da3065b630ce2a2c3ebfeebcc8299af52
Flávio Ricardo <flavioricardo@live.com>
2019-03-27 14:31:48 -0300

    Gerando itens de menu com componente Link

--- react-dashboard/src/components/MenuSuperior/MenuSuperior.js
+++ react-dashboard/src/components/MenuSuperior/MenuSuperior.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class MenuSuperior extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a href="/" className="navbar-brand">Dashboard</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-                    <span className="navbar-toggle-icon"></span>
+                <Link to="/" className="navbar-brand">Dashboard</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav mr-auto">
-                        <li className="nav-item"><a className="nav-link" href="/consultas">Consultas</a></li>
-                        <li className="nav-item"><a className="nav-link" href="/faturamento">Faturamento</a></li>
+                        <li className="nav-item"><Link to="/" className="nav-link">Resumo</Link></li>
+                        <li className="nav-item"><Link to="/consultas" className="nav-link">Consultas</Link></li>
+                        <li className="nav-item"><Link to="/faturamento" className="nav-link">Faturamento</Link></li>
                     </ul>
                 </div>
             </nav>
