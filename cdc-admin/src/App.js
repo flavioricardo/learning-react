9cf7717fcd93ab05ad6c2f815cce021650ae2bab
Flávio Ricardo <flavioricardo@live.com>
2019-03-28 16:58:09 -0300

    Criando formulário de cadastro de autores

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -7,10 +7,16 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			lista : []
+			lista : [],
+			nome : '',
+			email : '',
+			senha : ''
 		};
+		// Associa o this do método submitForm ao this do React
+		this.submitForm = this.submitForm.bind(this);
 	}
 
+	// Função só é chamada depois da primeira renderização
 	componentDidMount() {
 		fetch('http://localhost:8080/api/autores').then(
 			resultados => resultados.json().then(
@@ -28,6 +34,23 @@ class App extends Component {
 		// });
 	}
 
+	submitForm(event) {
+		event.preventDefault();
+		fetch('http://localhost:8080/api/autores', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				nome: this.state.nome,
+				email: this.state.email,
+				senha: this.state.senha
+			})
+		}).then(function(resultados) {
+			console.log(resultados.statusText);
+		})
+	}
+
 	render() {
 		return (
 			<div>
@@ -35,6 +58,25 @@ class App extends Component {
 				<div className="container">
 					<h2 className="mt-2">Cadastro de Autores</h2>
 					<div className="row">
+						<div className="col">
+							<form onSubmit={ this.submitForm }>
+								<div className="form-group">
+									<label htmlFor="nome">Nome</label>
+									<input type="text" className="form-control" id="nome" placeholder="Nome" value={ this.state.nome } onChange={ this.setNome } />
+								</div>
+								<div className="form-group">
+									<label htmlFor="email">E-mail</label>
+									<input type="email" className="form-control" id="email" placeholder="E-mail" value={ this.state.email } onChange={ this.setEmail } />
+								</div>
+								<div className="form-group">
+									<label htmlFor="senha">Senha</label>
+									<input type="password" className="form-control" id="senha" placeholder="Senha" value={ this.state.senha } onChange={ this.setSenha } />
+								</div>
+								<button type="submit" className="btn btn-primary">Enviar</button>
+							</form>
+						</div>
+					</div>
+					<div className="row mt-4">
 						<div className="col">
 							<table className="table table-bordered">
 								<thead className="thead-light">
