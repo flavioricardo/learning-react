81c92d544dc739c927d60bb3588c8bf4c74c04dd
Flávio Ricardo <flavioricardo@live.com>
2019-03-28 14:25:12 -0300

    Incluindo Bootstrap e criando menu superior

---
+++ cdc-admin/src/components/MenuSuperior/MenuSuperior.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class MenuSuperior extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <Link to="/" className="navbar-brand">CDC Admin</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav mr-auto">
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default MenuSuperior;
