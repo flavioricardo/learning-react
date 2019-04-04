20b946d750920cb8e7808ccfc91877efea34b5ae
Flávio Ricardo <flavioricardo@live.com>
2019-04-04 12:36:06 -0300

    Configurando rotas para cada componente

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import MenuSuperior from './components/MenuSuperior/MenuSuperior';
 import AutorBox from './components/Autor/Autor';
+import Livro from './components/Livro/Livro';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
 
@@ -9,7 +11,11 @@ class App extends Component {
 		return (
 			<div>
 				<MenuSuperior />
-				<AutorBox />
+				<Switch>
+					{/* <Route path="/" extact component={} /> */}
+					<Route path="/autores" component={AutorBox} />
+					<Route path="/livros" component={Livro} />
+				</Switch>
 			</div>
 		);
 	}
